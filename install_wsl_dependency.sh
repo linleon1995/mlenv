dfc1618ae52a801f59950a99fec7b045b2884579
Leonjs_lin <l84328g@gmail.com>
2025-04-11 16:51:46 +0800

    update wsl package script.

---
+++ install_wsl_dependency.sh
@@ -0,0 +1,21 @@
+sudo apt-get update && apt-get install -y \
+    tree \
+    htop \
+    openssl \
+    jq \
+    vim \
+    ssh \
+    curl \
+    bat \
+    fish \
+    tmux \
+    rsync \
+    wget \
+    git \
+    nano \
+    zip \
+    ffmpeg \
+    libxext6 \
+    libsm6 \
+    dialog \
+    apt-utils
